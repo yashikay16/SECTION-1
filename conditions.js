6f7297796f04f6d334e8588333ac18c319143f6a
yashikay16 <yashikay16@gmail.com>
2023-07-08 12:56:52 +0530

    upload

---
+++ conditions.js
@@ -0,0 +1,18 @@
+a=1234;
+if(a%2==0)
+{   myvar="nive"
+    var myvar1="using var"
+    let myvar2="using let keyw"
+    const myvar3="using const key"
+    console.log("Number is even");
+
+}
+else{
+    console.log("number is odd")
+};
+console.log(myvar)
+console.log(myvar1)
+console.log(myvar3)
+// console.log(myvar2)
+// variable scope is global for all not matter local and stufff 
+// jab func meim muilti blocks use var
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello JavaScript');
+num=10;  
+// semicolon optional
+console.log(num);
+myname="Yashika";
+loggedin="true"
+console.log(myname);
+console.log(typeof num);
